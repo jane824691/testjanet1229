4616fc2d4515401e1d6ab97ae07df016acdb30de
Janet <jane824691@gmail.com>
2024-01-17 23:30:10 +0800

    Update order-list.js

--- routes/order-list.js
+++ routes/order-list.js
@@ -49,7 +49,7 @@ const getListData = async (req) => {
   let where = ` WHERE 1 `;
   if (keyword) {
     qs.keyword = keyword;
-    where += ` AND ( \`name\` LIKE ${keyword_} OR \`mobile\` LIKE ${keyword_} ) `;
+    where += ` AND ( \`name\` LIKE ${keyword_} OR \`phone\` LIKE ${keyword_} ) `;
   }
   if (startDate) {
     qs.startDate = startDate;
@@ -136,32 +136,32 @@ router.post("/add", upload.none(), async (req, res) => {
     postData: req.body, // 除錯用
   };
 
-
   //前端叫什麼, 這邊要對應才能接收, sql才塞回table底下的欄位名稱
   const {
+    oid,
     name,
     coupon_id,
     discount,
     phone,
     email,
-    total,
+    netTotal,
     pay_way,
     postcode,
     address,
     delivery_way,
   } = req.body;
   const sql =
-    "INSERT INTO `order_list`(`order_name`, `order_phone`, `order_email`, `shipping_zipcode`, `shipping_address`, `order_date`) VALUES (?, ?, ?, ?, ?, NOW())";
+    "INSERT INTO `order_list`(`oid`, `order_name`, `order_phone`, `order_email`, `total`, `shipping_zipcode`, `shipping_address`, `order_date`) VALUES (?, ?, ?, ?, ?, ?, ?, NOW())";
 
-    //`coupon_id`, `discount`, `total`, `pay_way`,  delivery_way, 
   try {
     const [result] = await db.query(sql, [
+      oid,
       name,
       // coupon_id,
       // discount,
       phone,
       email,
-      // total,
+      netTotal,
       // pay_way,
       postcode,
       address,
@@ -169,6 +169,22 @@ router.post("/add", upload.none(), async (req, res) => {
     ]);
     output.result = result;
     output.success = !!result.affectedRows;
+    // const {
+    //   oid,
+    //   pid,
+    //   itemTotal, // (每項目種小計) 整體項目總計 = actual_amount
+    //   totalPrice //整體項目價錢總計
+    // } = req.body;
+    // const insertId =result.insertId
+    // const sql2 =
+    // "INSERT INTO `order_child`(`oid`, `pid`, `sale_price`, `actual_amount`) VALUES (?, ?, ?, ?)";
+    // const [result2] = await db.query(sql2,[
+    //   oid,
+    //   pid,
+    //   itemTotal,
+    //   totalPrice
+    //   //items.map((item) => [0, insertId,item.oid])
+    // ])
   } catch (ex) {
     output.exception = ex;
   }
